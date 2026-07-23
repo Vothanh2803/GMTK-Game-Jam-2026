ddf2c3f8e4948179dc78456db8fbe19f335e9a8b
Nguyen <clonecobth@gmail.com>
2026-07-23 23:58:30 +0700

    add hp

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -11,16 +11,38 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private int maxRage = 100;
     [SerializeField] private int currentRage;
 
+    [Header("Health")]
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+    public int CurrentHealth => currentHealth;
+    public int MaxHealth => maxHealth;
     public int CurrentActionPoint => currentActionPoint;
     public int MaxActionPoint => maxActionPoint;
 
     public int CurrentRage => currentRage;
     public int MaxRage => maxRage;
 
+
     private void Awake()
     {
+        currentHealth = maxHealth;
         currentActionPoint = maxActionPoint;
         currentRage = 0;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+    }
+
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
     }
 
     public void AddActionPoint(int amount)
